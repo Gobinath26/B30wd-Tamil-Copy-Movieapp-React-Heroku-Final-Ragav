dd364ece8aabf413b929c7318c14aed9366d3b66
ragavkumarv <ragavkumarv@gmail.com>
2022-03-17 11:06:30 +0530

    heroku url

--- src/global.js
+++ src/global.js
@@ -1,3 +1,4 @@
-export const API = "https://60c98aa8772a760017203b57.mockapi.io";
+// export const API = "https://60c98aa8772a760017203b57.mockapi.io";
+export const API = "https://b30wd-node.herokuapp.com";
 
 // const API = "https://my-json-server.typicode.com/ragavkumarv/fun-data";
